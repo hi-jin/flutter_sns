f7cfb79cbff0089fdd9c2b96c163a689ca908205
hi-jin <crushed7@o.cnu.ac.kr>
2022-05-09 00:24:01 +0900

    image upload method

--- lib/ui/sns_feed/feed_view.dart
+++ lib/ui/sns_feed/feed_view.dart
@@ -1,4 +1,9 @@
+import 'dart:io';
+
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_sns/widgets/colored_button.dart';
+import 'package:image_picker/image_picker.dart';
 
 import '../../core/auth.dart';
 
@@ -10,6 +15,9 @@ class FeedView extends StatefulWidget {
 }
 
 class _FeedViewState extends State<FeedView> {
+  final _fireStorage = FirebaseStorage.instance.ref();
+  final _imagePicker = ImagePicker();
+  File? _image;
 
   @override
   void initState() {
@@ -26,9 +34,38 @@ class _FeedViewState extends State<FeedView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        child: Text(
-            "hello world"
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 25, vertical: 10),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Expanded(
+                  child: Column(
+                children: [],
+              )),
+              ColoredButton(
+                title: "사진 업로드",
+                backgroundColor: Colors.blue,
+                onPressed: () async {
+                  XFile? image = await _imagePicker.pickImage(
+                      source: ImageSource.gallery,
+                      maxWidth: 400,
+                      maxHeight: 400);
+                  if (image == null) return;
+
+                  setState(() {
+                    _image = File(image.path);
+                  });
+                  await _fireStorage
+                      .child(
+                          'post/${user!.email}-${DateTime.now().millisecondsSinceEpoch}.png')
+                      .putFile(_image!)
+                      .whenComplete(() => print('hi'));
+                },
+              )
+            ],
+          ),
         ),
       ),
     );
